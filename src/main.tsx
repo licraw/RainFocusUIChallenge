b7a898083c56e09eba2ba6dd82b6fbf811b5f293
licraw <102079936+licraw@users.noreply.github.com>
2026-05-28 11:30:02 -0700

    comment out test brand css import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
-import './styles/testBrand.css';
+// import './styles/testBrand.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
